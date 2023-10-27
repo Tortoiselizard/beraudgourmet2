cb8d622d0a21b4f843b84e40c27c87c69b1d75bd
troy <troy00pernia@gmail.com>
2023-10-26 22:04:36 -0400

    add components: Footer and OrderRewards

--- src/app/menu/page.js
+++ src/app/menu/page.js
@@ -1,10 +1,14 @@
 import ContainerItems from '../../components/ContainerItems/ContainerItems'
+import OrderRewards from '../../components/OrderRewards/OrderRewards'
+import Footer from '@/components/Footer/Footer'
 
 function Menu () {
   return (
     <>
       <h1>Estoy en el Menu</h1>
       <ContainerItems />
+      <OrderRewards />
+      <Footer />
     </>
   )
 }
---
+++ src/components/OrderRewards/OrderRewards.jsx
@@ -0,0 +1,11 @@
+import style from './OrderRewards.module.css'
+
+function OrderRewards () {
+  return (
+    <div className={style.containerOrderRewards}>
+      OrderRewards
+    </div>
+  )
+}
+
+export default OrderRewards
